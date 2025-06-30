d00a3937c819e4f4c63709a3501ede865369423f
Abel <abonuoha@gmail.com>
2025-06-30 19:37:44 +0100

    Improve add board button

--- lib/screens/main/choose_board/index.dart
+++ lib/screens/main/choose_board/index.dart
@@ -67,12 +67,71 @@ class ChooseBoardScreen extends StatelessWidget {
           decoration: BoxDecoration(color: AppTheme.white),
           child: LayoutBuilder(
             builder: (_, constraints) {
+
+                bool isNarrow = constraints.maxWidth < 400; 
+                
               return ScrollableRow(
                 padding: EdgeInsets.symmetric(vertical: 10),
                 child: Container(
                   constraints: BoxConstraints(minWidth: constraints.maxWidth),
                   padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: Row(
+                  child:  isNarrow?  Column(
+                    spacing: 10,
+                    children: [
+                      Row(
+                        spacing: 10,
+                        children: [
+                          FlutterSwitch(
+                            width: 40,
+                            height: 22,
+                            toggleSize: 20,
+                            value: vm.saveAsFavoriteStyle,
+                            padding: 1,
+                            onToggle: vm.updateSaveAsFavoriteStyle,
+                            activeColor: AppTheme.vividRose,
+                            inactiveColor: AppTheme.lightGray,
+                          ),
+                          Text(
+                            'Save as Favorite Style',
+                            style: AppTheme.text.copyWith(
+                              color: AppTheme.vividRose,
+                              fontWeight: getFontWeight(500),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        spacing: 20,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          AppButton.secondary(
+                            wrapWithFlexible: true,
+                            mainAxisSize: MainAxisSize.min,
+                            onTap: vm.skipAndUseDefault,
+                            style: AppTheme.text.copyWith(
+                              color: AppTheme.vividRose,
+                              fontWeight: getFontWeight(500),
+                            ),
+                            minHeight: 35,
+                            color: AppTheme.vividRose,
+                            text: 'Skip & Use Default',
+                          ),
+                          AppButton(
+                            minHeight: 35,
+                            color: AppTheme.vividRose,
+                            wrapWithFlexible: true,
+                            mainAxisSize: MainAxisSize.min,
+                            onTap: vm.createBoard,
+                            text: 'Create Board',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ):
+                  
+                  
+                  
+                  Row(
                     spacing: 30,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
